a6c727d92eabf5aa7009278a13ee8de19f47ca29
mohammadshaez <mohammadshaez@gmail.com>
2026-07-10 17:16:15 +0530

    fixed env vars

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
 export const API_BASE = `${BACKEND_URL}/api`;
 
 export const api = axios.create({
