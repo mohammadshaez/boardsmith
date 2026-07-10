66745fb537dd94be7be64a10c356894cb82f6dee
mohammadshaez <mohammadshaez@gmail.com>
2026-07-10 20:09:25 +0530

    removed trailing slash

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // app.use(cors({ origin: true, credentials: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://boardsmith-rose.vercel.app/"],
+    origin: ["http://localhost:3000", "https://boardsmith-rose.vercel.app"],
     credentials: true,
   }),
 );
